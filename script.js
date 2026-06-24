08ad4bed9f1651bc8ac7281b8168c386e6400b06
siddesh-fx <boysiddesh7@gmail.com>
2026-06-24 15:18:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -387,3 +387,13 @@ ${project}`
     window.open(gmailUrl, "_blank");
 
 });
+
+
+const spotlight = document.querySelector('.spotlight');
+
+document.addEventListener('mousemove',(e)=>{
+
+    spotlight.style.left = e.clientX + 'px';
+    spotlight.style.top = e.clientY + 'px';
+
+});
